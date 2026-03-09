ca41eb5474af84443b8f731731c8a0cb01d1bdbd
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-09 18:40:20 -0300

    feat(api-gateway): adiciona tratamento de excepitions globalmente

---
+++ api-gateway/src/filters/all-exception.filter.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class allExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(allExceptionFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const message = exception?.response?.message || [exception?.message] || [
+        'Internal server error',
+      ];
+
+    let status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.BAD_REQUEST;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    if (exception?.message.includes('not found')) status = 404;
+
+    // console.log('exception: ', exception);
+    // console.log('ctx: ', ctx);
+    // console.log('response: ', response);
+    // console.log('request: ', request);
+
+    this.logger.error({
+      path: request.path,
+      status,
+      method: request.method,
+      message,
+    });
+
+    response.status(status).json({
+      statusCode: status,
+      timeStamp: new Date(),
+      path: request.path,
+      message,
+    });
+  }
+}
