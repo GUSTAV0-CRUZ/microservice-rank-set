71466737f70d96e1f13b425e6b6e4660e078cee2
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-18 14:51:23 -0300

    feat(micro-ranking): adiciona o repository com crud basico

--- micro-ranking/src/ranking/repository/ranking.repository.ts
+++ micro-ranking/src/ranking/repository/ranking.repository.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { RankingDocument } from '../schema/ranking.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateRankingDto } from '../dtos/create-ranking.dto';
+import { UpdateRankingDto } from '../dtos/update-ranking.dto';
 
 @Injectable()
-export class RankingRepository {}
+export class RankingRepository {
+  constructor(
+    @InjectModel('Ranking')
+    private readonly rankingModel: Model<RankingDocument>,
+  ) {}
+
+  findAll() {
+    return this.rankingModel.find();
+  }
+
+  findOneId(id: string) {
+    return this.rankingModel.findById(id).exec();
+  }
+
+  create(createrankingDto: CreateRankingDto) {
+    return this.rankingModel.create(createrankingDto);
+  }
+
+  update(id: string, updateRankingDto: UpdateRankingDto) {
+    return this.rankingModel
+      .findByIdAndUpdate(id, updateRankingDto, { returnDocument: 'after' })
+      .exec();
+  }
+
+  delete(id: string) {
+    return this.rankingModel.findByIdAndDelete(id).exec();
+  }
+}
