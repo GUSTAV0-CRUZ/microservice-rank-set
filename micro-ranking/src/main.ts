912df83d5201299484214cba3446e123f1fad848
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-18 10:45:29 -0300

    adiciona o novo microservice para cuidar do rank

---
+++ micro-ranking/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [String(process.env.RABBITMQ_URL)],
+        noAck: false,
+        queue: 'micro-ranking',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  logger.log('Initializing microservice micro-challenge...');
+  await app.listen();
+}
+bootstrap();
